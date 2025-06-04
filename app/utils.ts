90183e8778eb0fdfe22e4015f1c3f10152bcbb72
Ian Elliott <ianelliott@eandssolutions.com>
2025-06-04 14:56:31 -0500

    Table formatting adjustments Rev 2

--- app/utils.ts
+++ app/utils.ts
@@ -2,7 +2,7 @@ export const safeFixed = (val: number | undefined | null, digits: number) =>
   typeof val === "number" ? val.toFixed(digits) : "-";
 
 export const safePercent = (val: number | undefined | null, digits: number = 1) =>
-  typeof val === "number" ? `${val.toFixed(digits)}%` : "-";
+  typeof val === "number" ? `${(val *100).toFixed(digits)}%` : "-";
 
 export const safeNumber = (val: number | undefined | null) =>
   typeof val === "number" ? val.toLocaleString() : "-";
